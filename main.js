b47fb66c09b824ae7b5cd673eabeb0b5b4ae1765
Dagi <dagi.zewdu.dz@gmail.com>
2022-01-24 18:12:39 +0300

    Add hide menu when the links in mobile-nav are clicked

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const MOBILE_MAX_WIDTH = 992;
 const mobileMenuButton = document.querySelector(
   '.mobile .menu-button',
 );
-
+const mobileList=document.querySelectorAll('.mobile-list >li')
 const mobileMenu = document.querySelector(
   'div.mobile-menu',
 );
@@ -24,7 +24,12 @@ const hideMobileMenu = function (e) {
     mobileMenu.style.display = 'none';
     mobileMenu.style['z-index'] = -2;
   }
+ if(e.currentTarget.parentNode.classList.contains('mobile-list')){
+  mobileMenu.style.display = 'none';
+  mobileMenu.style['z-index'] = -2;
+ }
 };
 
 mobileMenuButton.addEventListener('click', showMobileMenu);
 cancelMobileMenu.addEventListener('click', hideMobileMenu);
+mobileList.forEach(node=> node.addEventListener('click',hideMobileMenu))
