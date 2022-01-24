fe1b62036b82f8f2e4c2b130ca68e94ad9be7002
sboursen <dev.sboursen@gmail.com>
2022-01-24 17:26:37 +0100

    Fix linter errors

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const MOBILE_MAX_WIDTH = 992;
-
 const mobileMenuButton = document.querySelector(
   '.mobile .menu-button',
 );
-const mobileList=document.querySelectorAll('.mobile-list >li')
+const mobileList = document.querySelectorAll(
+  '.mobile-list >li',
+);
 const mobileMenu = document.querySelector(
   'div.mobile-menu',
 );
@@ -12,24 +12,28 @@ const cancelMobileMenu = document.querySelector(
   '.mobile-menu .cancel',
 );
 
-const showMobileMenu = function (e) {
+function showMobileMenu(e) {
   if (e.currentTarget.classList.contains('menu-button')) {
     mobileMenu.style.display = 'flex';
     mobileMenu.style['z-index'] = 2;
   }
-};
+}
 
-const hideMobileMenu = function (e) {
+function hideMobileMenu(e) {
   if (e.currentTarget.classList.contains('cancel')) {
     mobileMenu.style.display = 'none';
     mobileMenu.style['z-index'] = -2;
   }
- if(e.currentTarget.parentNode.classList.contains('mobile-list')){
-  mobileMenu.style.display = 'none';
-  mobileMenu.style['z-index'] = -2;
- }
-};
+  if (
+    e.currentTarget.parentNode.classList.contains(
+      'mobile-list',
+    )
+  ) {
+    mobileMenu.style.display = 'none';
+    mobileMenu.style['z-index'] = -2;
+  }
+}
 
 mobileMenuButton.addEventListener('click', showMobileMenu);
 cancelMobileMenu.addEventListener('click', hideMobileMenu);
-mobileList.forEach(node=> node.addEventListener('click',hideMobileMenu))
+mobileList.forEach((node) => node.addEventListener('click', hideMobileMenu));
