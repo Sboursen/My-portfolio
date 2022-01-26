69a515cc9499dc84cd0e20c4e08d8aec620d1f1a
sboursen <dev.sboursen@gmail.com>
2022-01-26 14:00:44 +0100

    Add small placeholder to from

--- main.js
+++ main.js
@@ -1,3 +1,100 @@
+// |||Mobile menu
+const mobileMenuButton = document.querySelector(
+  '.mobile .menu-button',
+);
+const mobileMenuList = document.querySelectorAll(
+  '.mobile-list > li',
+);
+const mobileMenu = document.querySelector(
+  'div.mobile-menu',
+);
+
+const cancelMobileMenu = document.querySelector(
+  '.mobile-menu .cancel',
+);
+
+function showMobileMenu(e) {
+  if (e.currentTarget.classList.contains('menu-button')) {
+    mobileMenu.style.display = 'flex';
+    mobileMenu.style['z-index'] = 2;
+  }
+}
+
+function hideMobileMenu(e) {
+  if (
+    e.currentTarget.classList.contains('cancel') ||
+    e.currentTarget.parentNode.classList.contains(
+      'mobile-list',
+    )
+  ) {
+    mobileMenu.style.display = 'none';
+    mobileMenu.style['z-index'] = -2;
+  }
+}
+
+function hideMobileMenuOnScroll() {
+  if (mobileMenu.style.display !== 'none') {
+    mobileMenu.style.display = 'none';
+    mobileMenu.style['z-index'] = -2;
+  }
+}
+
+mobileMenuButton.addEventListener('click', showMobileMenu);
+cancelMobileMenu.addEventListener('click', hideMobileMenu);
+window.addEventListener('scroll', hideMobileMenuOnScroll);
+mobileMenuList.forEach((node) =>
+  node.addEventListener('click', hideMobileMenu),
+);
+
+// ||Scroll spy
+const projectSections = [
+  document.getElementById('hello-section'),
+  document.getElementById('works-section'),
+  document.getElementById('about-section'),
+  document.getElementById('contact-section'),
+];
+let sum = 0;
+let sectionHeights = projectSections.map((section) => {
+  sum += Number(
+    getComputedStyle(section).height.slice(0, -2),
+  );
+  return sum;
+});
+
+const desktopMenu = document.querySelector('.desktop');
+
+const desktopMenuList =
+  document.querySelectorAll('.desktop li');
+function updateSectionsHeight() {
+  sum = 0;
+  sectionHeights = projectSections.map((section) => {
+    sum += getComputedStyle(section).height;
+    return sum;
+  });
+}
+
+function spyOnScroll() {
+  if (getComputedStyle(desktopMenu).display === 'flex') {
+    let sectionIndex = 0;
+    for (let i = 0; i < sectionHeights.length; i += 1) {
+      if (window.scrollY >= sectionHeights[i]) {
+        sectionIndex = i;
+      }
+    }
+
+    desktopMenuList.forEach((node, index) => {
+      if (index === sectionIndex) {
+        node.style.borderBottom = 'thick solid #0000FF';
+      } else {
+        node.style.borderBottom = 'none';
+      }
+    });
+  }
+}
+
+window.addEventListener('scroll', spyOnScroll);
+window.addEventListener('resize', updateSectionsHeight);
+
 // ||Dynamic project details generation
 const projectsDetailsData = {
   'project-1': {
@@ -104,9 +201,11 @@ const projectDetailsCancelButton = document.querySelector(
 function showProjectDetails(e) {
   const projectId = e.currentTarget.id;
 
-  projectDetails.querySelector('.project-title').innerHTML = projectsDetailsData[projectId].title;
+  projectDetails.querySelector('.project-title').innerHTML =
+    projectsDetailsData[projectId].title;
 
-  projectDetails.querySelector('.project-image img').src = projectsDetailsData[projectId].featuredImage;
+  projectDetails.querySelector('.project-image img').src =
+    projectsDetailsData[projectId].featuredImage;
 
   projectDetails.querySelector(
     '.project-languages',
@@ -124,7 +223,8 @@ function showProjectDetails(e) {
 
   projectDetails.querySelector(
     '.project-description',
-  ).textContent = projectsDetailsData[projectId].description;
+  ).textContent =
+    projectsDetailsData[projectId].description;
 
   projectDetails.style.display = 'block';
 }
@@ -133,99 +233,49 @@ function hideProjectDetails() {
   projectDetails.style.display = 'none';
 }
 
-seeProjectButtons.forEach((button) => button.addEventListener('click', showProjectDetails));
+seeProjectButtons.forEach((button) =>
+  button.addEventListener('click', showProjectDetails),
+);
+
 projectDetailsCancelButton.addEventListener(
   'click',
   hideProjectDetails,
 );
 
-// ||Scroll spy
-const projectSections = [
-  document.getElementById('hello-section'),
-  document.getElementById('works-section'),
-  document.getElementById('about-section'),
-  document.getElementById('contact-section'),
-];
+// |||Validate contact form
+const isRequired = (value) => (value === '' ? false : true);
+const isBetween = (length, min, max) =>
+  length < min || length > max ? false : true;
+const isEmailValid = (email) => {
+  const pattern =
+    /^[a-z0-9._%+-]{3,}@[a-z0-9.-]{3,}(?:\.[a-z]{3,}){1,2}$/;
+  return pattern.test(email);
+};
+const contactForm = document.querySelector('form.form');
+const [nameField, emailField, messageField, submitButton] =
+  contactForm.children;
+console.log([
+  nameField,
+  emailField,
+  messageField,
+  submitButton,
+]);
 
-let sum = 0;
-let sectionHeights = projectSections.map((section) => {
-  sum += Number(
-    getComputedStyle(section).height.slice(0, -2),
-  );
-  return sum;
-});
-const desktopMenu = document.querySelector('.desktop');
-const desktopMenuList = document.querySelectorAll('.desktop li');
-const mobileMenuButton = document.querySelector(
-  '.mobile .menu-button',
+const showError = (field, message) => {
+  field.classList.remove('success');
+  field.classList.add('error');
+  const error = field.getElementsByTagName('small');
+  console.log(error);
+  error.textContent = message;
+};
+
+showError(nameField, 'test');
+
+function validateContactForm(e) {
+  e.preventDefault();
+}
+
+submitButton.addEventListener(
+  'submit',
+  validateContactForm,
 );
-const mobileMenuList = document.querySelectorAll(
-  '.mobile-list > li',
-);
-const mobileMenu = document.querySelector(
-  'div.mobile-menu',
-);
-
-const cancelMobileMenu = document.querySelector(
-  '.mobile-menu .cancel',
-);
-
-function showMobileMenu(e) {
-  if (e.currentTarget.classList.contains('menu-button')) {
-    mobileMenu.style.display = 'flex';
-    mobileMenu.style['z-index'] = 2;
-  }
-}
-
-function hideMobileMenu(e) {
-  if (
-    e.currentTarget.classList.contains('cancel')
-    || e.currentTarget.parentNode.classList.contains(
-      'mobile-list',
-    )
-  ) {
-    mobileMenu.style.display = 'none';
-    mobileMenu.style['z-index'] = -2;
-  }
-}
-
-function hideMobileMenuOnScroll() {
-  if (mobileMenu.style.display !== 'none') {
-    mobileMenu.style.display = 'none';
-    mobileMenu.style['z-index'] = -2;
-  }
-}
-
-function updateSectionsHeight() {
-  sum = 0;
-  sectionHeights = projectSections.map((section) => {
-    sum += getComputedStyle(section).height;
-    return sum;
-  });
-}
-
-function spyOnScroll() {
-  if (getComputedStyle(desktopMenu).display === 'flex') {
-    let sectionIndex = 0;
-    for (let i = 0; i < sectionHeights.length; i += 1) {
-      if (window.scrollY >= sectionHeights[i]) {
-        sectionIndex = i;
-      }
-    }
-
-    desktopMenuList.forEach((node, index) => {
-      if (index === sectionIndex) {
-        node.style.borderBottom = 'thick solid #0000FF';
-      } else {
-        node.style.borderBottom = 'none';
-      }
-    });
-  }
-}
-
-mobileMenuButton.addEventListener('click', showMobileMenu);
-cancelMobileMenu.addEventListener('click', hideMobileMenu);
-mobileMenuList.forEach((node) => node.addEventListener('click', hideMobileMenu));
-window.addEventListener('scroll', hideMobileMenuOnScroll);
-window.addEventListener('scroll', spyOnScroll);
-window.addEventListener('resize', updateSectionsHeight);
