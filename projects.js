61723a292fb76c3865ee7e477bdd122b0e334859
Soufiane Boursen <dev.sboursen@gmail.com>
2022-08-02 13:01:52 +0100

    Supply Project data

--- projects.js
+++ projects.js
@@ -1,12 +1,77 @@
 const projectsData = {
+  'project-9': {
+    title: 'al-mentoria',
+    featuredImage: './resources/al-mentoria.png',
+    technologies: ['React', 'ReduxToolKit', 'Rails', 'Devise', 'TailWindCss', 'Rspec', 'JEST', 'Postgresql'],
+    liveLink: 'https://al-mentoria.herokuapp.com/',
+    sourceLink: 'https://github.com/UpliftLab',
+    description: 'Al Mentoria is a virtual space to connect with mentors that can help you with your technical as well as behavioral issues. If you are stuck somewhere and need a second look at things, make a reservation now.',
+  },
+  'project-8': {
+    title: 'Portefeuille',
+    featuredImage: './resources/portfeuille.png',
+    technologies: ['Rails', 'Devise', 'TailWindCss', 'Rspec', 'Daisyui', 'Postgresql'],
+    liveLink: 'https://rails-portefeuille.herokuapp.com/',
+    sourceLink: 'https://github.com/Sboursen/rails-capstone',
+    description: 'The Portefeuille is a budget tracking app. After creating an account, it allows to add your daily transactions given some categories. It also allows you add your own categories and list all the transactions belonging to that particular category.',
+  },
+  'project-7': {
+    title: 'Planètes-et-astéroïdes',
+    featuredImage: './resources/planettes.png',
+    technologies: ['React', 'Redux', 'Git', 'React testing library', 'The solar system API'],
+    liveLink: 'https://planetes-et-asteroides.netlify.app/',
+    sourceLink: 'https://github.com/Sboursen/planetes-et-asteroides',
+    description: 'Planètes-et-astéroïdes is a web app powered by the The solar system API that displays metrics relative to the planets, satellites, and asteroids that constitute our solar system. In addition to the ability to navigate between the different celestial bodies, it also offers a powerful search by name functionality. It is made as capstone project of the React&Redux module.',
+  },
+  'project-6': {
+    title: 'math-magicians',
+    featuredImage: './resources/math-magicians.gif',
+    technologies: ['React', 'SCSS', 'Git', 'React testing library', 'LocalStorage'],
+    liveLink: 'https://sboursen-math-magicians.netlify.app/',
+    sourceLink: 'https://github.com/Sboursen/math-magicians',
+    description: 'Math Magicians is an SPA made for all fans of maths. It contains a calculator that allows the users to make the majority of arithmetic operations. In additions, it displays a random math related quote in each session.',
+  },
+  'project-5': {
+    title: 'SpaceTravelers',
+    featuredImage: './resources/space-travelers.gif',
+    technologies: ['React', 'Redux', 'TailWindCss', 'Git', 'React testing library', 'SpaceX API', 'pair-programming'],
+    liveLink: 'https://harlexkhal.github.io/SpaceTravelers/',
+    sourceLink: 'https://github.com/Sboursen/SpaceTravelers',
+    description: 'Space Travelers Hub is a web application made for a company that provides commercial and scientific space travel services. The application allows users (future clients) to book rockets and join selected space missions.',
+  },
+  'project-4': {
+    title: 'Leaderboard',
+    featuredImage: './resources/Leaderbord.png',
+    technologies: ['HTML', 'TailWindCss', 'ES6 JavaScript', 'Git', 'Webpack'],
+    liveLink: 'https://sboursen.github.io/Leaderboard/',
+    sourceLink: 'https://github.com/Sboursen/Leaderboard',
+    description: 'Leaderboard is website that displays the scores submitted by all players. It also allows the current user to submit his/her score. All data is preserved thanks to the external Leaderboard API service.',
+  },
+  'project-3': {
+    title: 'TO DO LIST',
+    featuredImage: './resources/to-do-list.gif',
+    technologies: ['HTML', 'TailWindCss', 'ES6 JavaScript', 'Git', 'Webpack'],
+    liveLink: 'https://sboursen.github.io/to-do-tasks/',
+    sourceLink: 'https://github.com/Sboursen/to-do-tasks',
+    description: 'To-do list is a tool that helps you to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.',
+  },
+  'project-2': {
+    title: 'Awesome books 2B edition',
+    featuredImage: './resources/AwesomeBook2B.gif',
+    technologies: ['HTML', 'TailWindCss', 'ES6 JavaScript', 'Git', 'Figma'],
+    liveLink: 'https://sboursen.github.io/Awesome-Books-2BEdition/',
+    sourceLink: 'https://github.com/Sboursen/Awesome-Books-2BEdition',
+    description: 'Awesome books is simple web app that allows the user to create their own collection of books. The 2BEdition Uses a UI design heavily inspired by the award wining Nier Automata video game.',
+  },
   'project-1': {
     title: 'Chess World Conference CWC',
     featuredImage: './resources/project-image1.png',
-    technologies: ['HTML', 'CSS', 'JavaScript', 'Github', 'Figma'],
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Git', 'Figma'],
     liveLink: 'https://sboursen.github.io/Chess-World-Championship/',
     sourceLink: 'https://github.com/Sboursen/Chess-World-Championship',
     description: 'This is a website for a fictional Chess world conference. It\'s the HTML, CSS and basic JavaScript module capstone project.',
   },
+
 };
 
 export default projectsData;
